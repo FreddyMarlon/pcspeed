1e74cff51c00f4d7568e39d02b99499973be7fae
FreddyLopez <marlonf73@gmail.com>
2020-04-01 08:59:44 -0400

    Added a Procfile.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT=process.env.PORT;
 
 
 //configuraciones
 
-app.set('port',3000);
+app.set('port'PORT);
 app.set('views',path.join(__dirname,'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine','ejs');
