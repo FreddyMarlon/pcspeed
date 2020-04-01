3c7700400b4647f07098618217c94f5621c5c4b7
FreddyLopez <marlonf73@gmail.com>
2020-04-01 00:17:32 -0400

    mLab

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 
 //configuraciones
-app.set('port',3000);
+var port=normalizePort(process.env.PORT || '3000')
+app.set('port',port);
 app.set('views',path.join(__dirname,'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine','ejs');
