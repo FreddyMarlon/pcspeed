962f2695af6c2377aec42a557866d34cd573043f
FreddyLopez <marlonf73@gmail.com>
2022-02-15 20:07:29 -0400

    mejorado

--- src/routes/enlaces.js
+++ src/routes/enlaces.js
@@ -58,11 +58,32 @@ router.get('/edit/:id', isLoggedIn, async (req, res) => {
        req.flash('success','Solicitud actualizada correctamente');
        res.redirect('/enlaces');
  });
+
  
- router.get('/precios', async (req, res) => {        
-    const links = await pool.query('SELECT * FROM listaprecios');
+ router.get('/actualprecios',isLoggedIn, async (req, res) => {        
+    const links = await pool.query('SELECT * FROM ajustesprecios');
+    res.render('links/actualprecios',{link: links[0]});
+ });
+
+ 
+ router.post('/actualprecios',isLoggedIn, async (req, res) => {
+    const { tasacambio,costos} = req.body;
+    const newlink = {        
+        tasacambio,
+        costos
+    };    
+      await pool.query('UPDATE ajustesprecios SET ? WHERE id=1',[newlink]);             
+      await pool.query('COMMIT');
+      await pool.query('UPDATE listaprecios,ajustesprecios SET listaprecios.preciob=(listaprecios.preciob+((listaprecios.preciob*ajustesprecios.costos)/100))');      
+      await pool.query('COMMIT');
+      await pool.query('UPDATE listaprecios,ajustesprecios SET listaprecios.preciod=listaprecios.preciob/ajustesprecios.tasacambio;');
+       req.flash('success','Precios actualizados correctamente');             
+       res.redirect('/enlaces/precios');
+});
+
+ router.get('/precios', async (req, res) => {      
+    const links = await pool.query('SELECT listaprecios.id as id,listaprecios.nom,listaprecios.des,listaprecios.preciod,listaprecios.preciob,listaprecios.categoria,ajustesprecios.tasacambio,ajustesprecios.costos FROM listaprecios,ajustesprecios');
     res.render('links/precios',{links});
-    
  }); 
 
 module.exports = router;
