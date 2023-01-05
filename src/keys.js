03c7671e38cf615a421f257d9b6fbe0b5894e532
FreddyLopez <marlonf73@gmail.com>
2023-01-04 18:59:08 -0600

    mejorado

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports={
 	database:{
-		 host:'blo4ilbr1tvvnzsqprpx-mysql.services.clever-cloud.com',
-		 user:'uekvbv2uistitlxk',
-		 password:'JsdzfIZA7CR3BvTApyih',
-		 database:'blo4ilbr1tvvnzsqprpx'
+		 host:'sql9.freesqldatabase.com',
+		 user:'sql9588308',
+		 password:'H6EPU3stMM',
+		 database:'sql9588308'
 
 	}
 	/*database:{
